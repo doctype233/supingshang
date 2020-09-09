005cf18349c8f19ba27b4f672b7ce0808b152ea5
qinjiajia <17521350102@163.com>
2020-09-09 16:31:12 +0800

    home done

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -31,11 +31,9 @@ class PageNavbar extends React.Component {
                 <div className="logo"><img src={logo}/></div>
                 <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['0']} className={`header-menu`} id='bg-change'>
                     <Menu.Item key="0"><Link to='/'>首页</Link></Menu.Item>
-                    <Menu.Item key="1"><Link to='/story'>品牌故事</Link></Menu.Item>
                     <Menu.Item key="2"><Link to='/product'>产品列表</Link></Menu.Item>
                     <Menu.Item key="3"><Link to='/nav3'>在线提货</Link></Menu.Item>
                     <Menu.Item key="4"><Link to='/nav4'>售后保障</Link></Menu.Item>
-                    <Menu.Item key="5"><Link to='/nav5'>行业咨询</Link></Menu.Item>
                     <Menu.Item key="6"><Link to='/nav6'>联系我们</Link></Menu.Item>
                 </Menu>
             </Header>
--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,6 @@
  */
 import React from 'react';
 import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
-import StoryPage from './pages/story/index';
 import ProductPage from './pages/product/index';
 import Home from './pages/home/index';
 
@@ -22,7 +21,6 @@ class Routes extends React.Component {
                 location={location}
             >
                 <Route path='/index' component={Home} />
-                <Route path='/story' component={StoryPage} />
                 <Route path='/product' component={ProductPage} />
                 <Redirect path="/" to="/index" />
                 { /* 404 */}
